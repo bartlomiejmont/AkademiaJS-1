d1a80aaedab86d50e5276cde419fa28a5da2d2ca
Bartłomiej Mont <spankertroll77@gmail.com>
2020-10-26 23:51:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+let string = "tekst"
+let int = 5
+let float = 5.5
+let bool = true
+let unknown = undefined
+let nic = null
+
+
+console.log(typeof string)
+console.log(typeof int)
+console.log(typeof float)
+console.log(typeof bool)
+console.log(typeof unknown)
+console.log(typeof nic)
